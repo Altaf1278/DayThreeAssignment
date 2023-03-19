e8a066ef6e86620657537b1aae2445e9cd51b41a
altaf <altaf98khan66@gmail.com>
2023-03-19 22:35:24 +0530

    Added: Switch Case Problem

---
+++ src/com/bridgelabz/SwitchCaseEmployeeDailyWage.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.employeewageproblem;
+
+public class SwitchCaseEmployeeDailyWage {
+	public static void main(String[] args) {
+		int employeewageperhour = 20;
+		int fulldayhours = 8;
+		int parttime = 4;
+		 int dailywage =0;
+		 int hours = 4;
+		 switch (hours) {
+            case 8:
+                dailywage = employeewageperhour * fulldayhours;
+                System.out.println("Employee fullday salary" +dailywage);
+                break;
+            case 4:
+                dailywage = employeewageperhour * parttime;
+                System.out.println("Employee parttime salary" +dailywage);
+                break;
+            default:
+                System.out.println("Employee is Absent");
+                break;
+        } 
+	        System.out.println("Total salary: " +dailywage);
+	        
+	        
+	}
+
+}
