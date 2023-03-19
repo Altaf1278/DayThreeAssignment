b4a2298510ddd9a7d1b914800c3e6629c4218392
altaf <altaf98khan66@gmail.com>
2023-03-19 22:20:25 +0530

    Added: Daily Employee Wage Problem

---
+++ src/com/bridgelabz/DailyEmployeeWage.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.employeewageproblem;
+
+public class DailyEmployeeWage {
+	public static void main(String[] args) {
+		
+		/* Let us consider employee wage per hour =20
+		 *  Employee works in a day = 8hours
+		 */
+		
+	  int employeewageperhour = 20;
+	  int fulldayhours = 8;
+	  int dailyemployeewage = employeewageperhour * fulldayhours ;
+	  System.out.println("Daily Employee wage will be " +dailyemployeewage);
+	}
+
+}
